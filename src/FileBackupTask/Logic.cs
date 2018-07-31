1fc9efa5e79feb4503d118b6b03c488e46a272dd
Jason E <jason.eades@penskeracing.com>
2018-07-31 09:47:05 -0400

    Initial commit of project files.

---
+++ src/FileBackupTask/Logic.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace GenericFileBackupTask
+{
+    /// <summary>
+    /// The file backup task.
+    /// </summary>
+    public class FileBackup
+    {
+        #region Creation  	---------------------------------------------------------------
+
+        /// <summary>
+        /// Handles backing up a file.
+        /// </summary>
+        /// <param name="sourceFile">Source file to backup.</param>
+        /// <param name="backupLocation">Location to backup</param>
+        public FileBackup(string sourceFile, string backupLocation)
+        {
+            if (string.IsNullOrWhiteSpace(sourceFile))
+                throw new ArgumentException("Source File is a required value.", nameof(sourceFile));
+
+            if (!File.Exists(sourceFile))
+                throw new ArgumentException($"Source File does not exist: { sourceFile } or is not a file.", nameof(sourceFile));
+
+            if (string.IsNullOrWhiteSpace(backupLocation))
+                throw new ArgumentException("Backup location is a required value.", nameof(backupLocation));
+
+            if (!Directory.Exists(backupLocation))
+                throw new ArgumentException($"Backup location does not exist: { backupLocation } or is not a valid directory.", nameof(backupLocation));
+
+            this.SourceFile = sourceFile;
+            this.BackupFolder = backupLocation;
+        }
+
+        #endregion // Creation	---------------------------------------------------------------
+
+        #region Properties ---------------------------------------------------------------
+        
+        /// <summary>
+        /// The source file to make a backup of.
+        /// </summary>
+        private string SourceFile { get; set; }
+
+        /// <summary>
+        /// The folder where the backup should be saved.
+        /// </summary>
+        private string BackupFolder { get; set; }
+
+        #endregion // Properties	---------------------------------------------------------------
+
+        #region Public Methods	---------------------------------------------------------------
+
+        /// <summary>
+        /// Backs up a given source file to a backup folder with timestamp prefix and _Backup suffix.
+        /// </summary>
+        public void Backup()
+        {
+            string currentFileName = Path.GetFileNameWithoutExtension(SourceFile);
+            string currentFileExtension = Path.GetExtension(SourceFile);
+            string dateStamp = DateTime.Now.ToString("yyyyMMddHHmmss");
+            string backupFileName = string.Format("{0}{1}_Backup{2}", dateStamp, currentFileName, currentFileExtension);
+            string backupFileFull = Path.Combine(BackupFolder, backupFileName);
+            File.Copy(SourceFile, backupFileFull, false);
+        }
+
+        #endregion // Public Methods	---------------------------------------------------------------
+
+    }
+}
---
+++ src/FileBackupTask/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace GenericFileBackupTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string src = string.Format("{0}Source", string.IsNullOrEmpty(args[0]) ? "Generic" : args[0]);
+            string log = string.Format("{0}Log", src);
+
+            // Create the source, if it does not already exist.
+            if (!EventLog.SourceExists(src))
+            {
+                EventLog.CreateEventSource(src, log);                
+            }
+
+            // Create an EventLog instance and assign its source.
+            EventLog myLog = new EventLog();
+            myLog.Source = src;
+
+            // Write an informational entry to the event log.    
+            myLog.WriteEntry($"{src} backup program started.");
+            
+            if (args.Length != 3)
+                myLog.WriteEntry("Invalid number of arguments to start program.  Requires 3, received: " + args.Length, EventLogEntryType.Error);
+            
+            try
+            {
+                var logicInstance = new FileBackup(args[1], args[2]);
+                logicInstance.Backup();
+            }            
+            catch (Exception ex)
+            {
+                myLog.WriteEntry($"Error running program logic: {ex.Message}", EventLogEntryType.Error);
+                throw;
+            }
+        }
+    }
+}
